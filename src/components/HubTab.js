3ecbd34c5dc75c99e94c3e1a8dfcc04698a0262f
Ali Moallim <amoallim15@gmail.com>
2021-10-25 13:53:11 +0900

    commit 4.1

--- src/components/HubTab.js
+++ src/components/HubTab.js
@@ -1,59 +1,77 @@
-import React, { useState} from 'react';
+import React, { useState } from "react"
 import HubPage1 from "./HubPage1__1"
 import HubPage2 from "./HubPage1__2"
 
 const HubTab = () => {
-    const [openTab, setOpenTab] = useState(1);
+  const [openTab, setOpenTab] = useState(1)
 
-    return (
-        <div className="">
-            <div className="max-w-7xl mx-auto">
-                <h2 className="text-4xl mb-8 font-bold mt-8 text-center">창업 허브</h2>
-                <ul className="lg:grid lg:grid-cols-2 leading-10 my-4 text-center hidden">
-                    <li className={
-                        "text-md " + (openTab === 1 ? "border-b-2 border-black" : "border-b-2 border-gray-300")
-                        }>
-                        <a className={
-                            "block cursor-pointer " + (openTab === 1 ? "font-bold" : "text-gray-800")}
-                        onClick={(e) => {
-                            e.preventDefault();
-                            setOpenTab(1);
-                        }}
-                        data-goggle = "tab"
-                        herf = "#link1"
-                        role = "tablist">
-                            프랜차이즈 창업
-                        </a>
-                    </li>
-                    <li className={
-                        "text-md " + (openTab === 2 ? "border-b-2 border-black" : "border-b-2 border-gray-300")
-                        }>
-                        <a className={
-                            "block cursor-pointer " + (openTab === 2 ? "font-bold" : "text-gray-800")}
-                        onClick={(e) => {
-                            e.preventDefault();
-                            setOpenTab(2);
-                        }}
-                        data-goggle = "tab"
-                        herf = "#link1"
-                        role = "tablist">
-                            팔자 매거진
-                        </a>
-                    </li>
-                </ul>
-                <div>
-                    <div className="">
-                        <div className={openTab === 1?"block" : "hidden"} id="link1">
-                        <HubPage1 />
-                        </div>
-                        <div className={openTab === 2?"block" : "hidden"} id="link2">
-                            <HubPage2 />
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="">
+      <div className="max-w-7xl mx-auto">
+        <h2 className="text-4xl mb-8 font-bold mt-8 text-center">창업 허브</h2>
+        <ul className="lg:grid lg:grid-cols-2 leading-10 my-4 text-center hidden">
+          <li
+            className={
+              "text-md " +
+              (openTab === 1
+                ? "border-b-2 border-black"
+                : "border-b-2 border-gray-300")
+            }
+          >
+            <a
+              className={
+                "block cursor-pointer " +
+                (openTab === 1 ? "font-bold" : "text-gray-800")
+              }
+              onClick={(e) => {
+                e.preventDefault()
+                setOpenTab(1)
+              }}
+              data-goggle="tab"
+              herf="#link1"
+              role="tablist"
+            >
+              프랜차이즈 창업
+            </a>
+          </li>
+          <li
+            className={
+              "text-md " +
+              (openTab === 2
+                ? "border-b-2 border-black"
+                : "border-b-2 border-gray-300")
+            }
+          >
+            <a
+              className={
+                "block cursor-pointer " +
+                (openTab === 2 ? "font-bold" : "text-gray-800")
+              }
+              onClick={(e) => {
+                e.preventDefault()
+                setOpenTab(2)
+              }}
+              data-goggle="tab"
+              herf="#link1"
+              role="tablist"
+            >
+              팔자 매거진
+            </a>
+          </li>
+        </ul>
+        <div>
+          <div className="">
+            <div className={openTab === 1 ? "block" : "hidden"} id="link1">
+              <HubPage1 />
             </div>
+            <div className={openTab === 2 ? "block" : "hidden"} id="link2">
+              <HubPage2 />
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
-export default HubTab;
+export default HubTab
--- src/views/aboutView.js
+++ src/views/aboutView.js
@@ -1,30 +1,29 @@
 import React from "react"
 import HomeAppBar from "../components/homeAppBar"
-import Carousel from 'nuka-carousel';
-import topBannerSlideOne from "../assets/imgs/topban1.png";
-import topBannerSlideTwo from "../assets/imgs/topban2.jpg";
-
-
+// import Carousel from 'nuka-carousel';
+import topBannerSlideOne from "../assets/imgs/topban1.png"
+import topBannerSlideTwo from "../assets/imgs/topban2.jpg"
 
 export default function AboutView({ isAuth, userRole, userType }) {
   //
-  return (
-    <>
-    <div className="lg:block hidden">
-      <Carousel autoplay={true} withoutControls= {true} wrapAround = {true} initialSlideHeight = {80}>
-      <img className="block w-auto" src={topBannerSlideOne} alt="topban1" />
-      <img className="block w-auto" src={topBannerSlideTwo} alt="topban2" />
-      </Carousel>
-      </div>
-      <HomeAppBar
-        sticky={true}
-        isAuth={isAuth}
-        userRole={userRole}
-        userType={userType}
-      />
-      {/*   UPDATE HERE   */}
+  // return (
+  //   <>
+  //   <div className="lg:block hidden">
+  //     <Carousel autoplay={true} withoutControls= {true} wrapAround = {true} initialSlideHeight = {80}>
+  //     <img className="block w-auto" src={topBannerSlideOne} alt="topban1" />
+  //     <img className="block w-auto" src={topBannerSlideTwo} alt="topban2" />
+  //     </Carousel>
+  //     </div>
+  //     <HomeAppBar
+  //       sticky={true}
+  //       isAuth={isAuth}
+  //       userRole={userRole}
+  //       userType={userType}
+  //     />
+  //     {/*   UPDATE HERE   */}
 
-    </>
-  )
+  //   </>
+  // )
+  return <div />
 }
-// 
+//
